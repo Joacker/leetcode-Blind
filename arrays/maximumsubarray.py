19e74debbfe4fe23221dcd1665a956f9b2613b5b
Joacker <jfernandezvizcarra@gmail.com>
2025-10-25 17:26:42 -0300

    adding exercises for study

--- arrays/maximumsubarray.py
+++ arrays/maximumsubarray.py
@@ -15,15 +15,38 @@ def bruteforce_max_subarray(arr):
 def max_subarray(arr):
     max_sum = float('-inf')
     current_sum = 0
+    
+    start = 0
+    end = 0
+    temp_start = 0
+    
     for i in range(len(arr)):
         current_sum += arr[i]
+        
         if current_sum > max_sum:
             max_sum = current_sum
-            sub_end = i
+            start = temp_start
+            end = i
+        
         if current_sum < 0:
             current_sum = 0
-            sub_start = i + 1
-    return arr[sub_start:sub_end + 1]
+            temp_start = i + 1
+            
+    return arr[start:end+1]
+
+def maxSubArray(arr):
+    maxS = arr[0]
+    curS = 0
+    
+    for num in arr:
+        if curS < 0:
+            curS = 0
+        curS += num
+        if curS > maxS:
+            maxS = curS
+
+    return maxS
 
 print(bruteforce_max_subarray([-2,1,-3,4,-1,2,1,-5,4]))
 print(max_subarray([-2,1,-3,4,-1,2,1,-5,4]))
+print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
